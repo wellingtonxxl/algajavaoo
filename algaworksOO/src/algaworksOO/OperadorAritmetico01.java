dfa79fdbd88a2c546ddc0cafabe2f488960e05ab
Wellington Rodrigues <wellington.acr@gmail.com>
2021-08-18 19:21:21 -0400

    first commit

---
+++ algaworksOO/src/algaworksOO/OperadorAritmetico01.java
@@ -0,0 +1,17 @@
+package algaworksOO;
+
+public class OperadorAritmetico01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int notaAluno1 = 99;
+		int notaAluno2 = 67;
+		int notaAluno3 = 78;
+		int notaAluno4 = 84;
+		
+		int totalGeral = notaAluno1 + notaAluno2 + notaAluno3 + notaAluno4;
+		System.out.println("Nota geral: "+totalGeral);
+
+	}
+
+}
