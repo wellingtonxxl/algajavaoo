b31093ab23cecc2a6bf1a58a500ef872f6c4bff3
Wellington Rodrigues <wellington.acr@gmail.com>
2021-08-18 20:04:18 -0400

    Add Class PrecoMedioBombom

---
+++ algaworksOO/src/algaworksOO/PrecoMedioBombom.java
@@ -0,0 +1,21 @@
+package algaworksOO;
+
+public class PrecoMedioBombom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int preco1 = 5;
+		int preco2 = 8;
+		int preco3 = 12;
+		
+		int mediaPrecos = (preco1 + preco2 + preco3)/3;
+		
+		System.out.println("\"Sistema de Calculo de Media\"");
+		System.out.println("Preco 1: " +preco1);
+		System.out.println("Preco 2: " +preco2);
+		System.out.println("Preco 3: " +preco3);
+		System.out.println("\n Media dos precos:\n" +mediaPrecos);
+	}
+
+}
