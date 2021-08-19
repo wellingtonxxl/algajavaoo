b36c4f6f3a778c03962e6fd11fddbe7127c7889b
Wellington Rodrigues <wellington.acr@gmail.com>
2021-08-18 20:15:15 -0400

    Add Class TipoPrimitivo

---
+++ algaworksOO/src/algaworksOO/TipoPrimitivo.java
@@ -0,0 +1,22 @@
+package algaworksOO;
+
+public class TipoPrimitivo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double precoProduto = 9.43;
+		boolean alunoMatriculado = true;
+		boolean clienteBloqueado = false;
+		
+		char turmaAluno1 = 'A';
+		char tipoCliente = '2';
+		char simbolo = '@';
+		
+		long populacaoUberlandia = 650000;
+		
+		long populacaoMundial = 7000000000L;
+		
+		float saldoConta = 1030.43F;
+	}
+
+}
