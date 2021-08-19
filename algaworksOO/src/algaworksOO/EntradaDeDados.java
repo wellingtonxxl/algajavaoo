6a627d8142363fa33a92c3409ba705e51c8b8a83
Wellington Rodrigues <wellington.acr@gmail.com>
2021-08-19 15:44:56 -0400

    Adicionando estrutura condicional

--- algaworksOO/src/algaworksOO/EntradaDeDados.java
+++ algaworksOO/src/algaworksOO/EntradaDeDados.java
@@ -20,5 +20,17 @@ public class EntradaDeDados {
 		double imc = peso / (altura * altura);
 		
 		System.out.println("IMC de : "+nome +": " +imc);
+		
+		if (imc < 18.5) {
+			System.out.println("Abaixo do peso ideal");
+		}else if (imc < 25) {
+			System.out.println("Peso ideal");
+		}else if (imc < 30) {
+			System.out.println("Acima do peso");
+		}else if (imc < 40) {
+			System.out.println("Obesidade grau 1 ou 2");
+		}else {
+			System.out.println("Obesidade grau 3");
+		}
 	}
 }
