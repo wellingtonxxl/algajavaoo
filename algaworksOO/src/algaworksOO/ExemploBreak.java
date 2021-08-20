e437b0f0945f996e52cea36aa68553923ea445d6
Wellington Rodrigues <wellington.acr@gmail.com>
2021-08-19 20:38:30 -0400

    Add Class ExemploBreak e Continue

---
+++ algaworksOO/src/algaworksOO/ExemploBreak.java
@@ -0,0 +1,26 @@
+package algaworksOO;
+
+import java.util.Scanner;
+
+public class ExemploBreak {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Digite um numero: ");
+		int divisor = entrada.nextInt();
+		
+		for (int i = 100; i <= 200; i++) {
+			if (i % divisor == 0) {
+				//break;
+				continue;
+			}
+			
+			System.out.println(i);
+		}
+		
+		System.out.println("Fim do programa!");
+	}	
+
+}
