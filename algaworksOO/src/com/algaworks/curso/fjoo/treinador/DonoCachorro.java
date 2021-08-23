dd82ffe96f50d020f014ea144788dcec159aaa94
Wellington Rodrigues <wellington.acr@gmail.com>
2021-08-23 19:08:11 -0400

    Add modificador de acesso default

---
+++ algaworksOO/src/com/algaworks/curso/fjoo/treinador/DonoCachorro.java
@@ -0,0 +1,13 @@
+package com.algaworks.curso.fjoo.treinador;
+
+import com.algaworks.curso.fjoo.animal.Cachorro;
+
+public class DonoCachorro {
+
+	void ensinarCachorroSentar(Cachorro cachorro) {
+		
+		cachorro.sentar();
+		
+	}
+	
+}
