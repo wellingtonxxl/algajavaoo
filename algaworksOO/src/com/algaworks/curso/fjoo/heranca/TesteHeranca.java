446a34bf4fc35c43276c503e1cfeb4d89a80e572
Wellington Rodrigues <wellington.acr@gmail.com>
2021-08-23 21:12:04 -0400

    Add Heranca e Protected

---
+++ algaworksOO/src/com/algaworks/curso/fjoo/heranca/TesteHeranca.java
@@ -0,0 +1,16 @@
+package com.algaworks.curso.fjoo.heranca;
+
+public class TesteHeranca {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Jogador j = new Jogador();
+		
+		j.nome = "Neymar";
+		j.idade = 27;
+		
+		j.seApresentar();
+		j.dizerSeAindaJoga();
+	}
+
+}
