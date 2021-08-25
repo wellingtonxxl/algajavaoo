77598586a149b6ac4adaeca3ff25935dfa8497fe
Wellington Rodrigues <wellington.acr@gmail.com>
2021-08-24 20:56:12 -0400

    Add Trabalhando com Datas

---
+++ algaworksOO/src/com/algaworks/curso/fjoo/datas/TesteCalendar.java
@@ -0,0 +1,33 @@
+package com.algaworks.curso.fjoo.datas;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class TesteCalendar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calendar c = new GregorianCalendar();
+		
+		c.set(Calendar.DAY_OF_MONTH, 31);
+		c.set(Calendar.MONTH, 0); //mes começa no 0
+		c.set(Calendar.YEAR, 2021);
+		c.set(Calendar.HOUR_OF_DAY, 10);
+		c.set(Calendar.MINUTE, 30);
+		c.set(Calendar.SECOND, 0);
+		
+		DateFormat formatador = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		System.out.println(formatador.format(c.getTime()));
+		
+		c.add(Calendar.MONTH, 1);
+		System.out.println(formatador.format(c.getTime()));
+		
+		c.roll(Calendar.DAY_OF_MONTH, 1);
+		System.out.println(formatador.format(c.getTime()));
+		
+	}
+
+}
