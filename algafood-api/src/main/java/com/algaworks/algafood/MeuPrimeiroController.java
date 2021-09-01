ce58d478f4cb72a60fc2e0676cf932ea1aad9439
Wellington Rodrigues <wellington.acr@gmail.com>
2021-08-31 21:28:19 -0300

    Add Injeção de dependencias com beans spring

--- algafood-api/src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -4,12 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.service.AtivacaoClienteService;
+
 @Controller
 public class MeuPrimeiroController {
+
+	private AtivacaoClienteService ativacaoClienteService;
+	
+	
+	public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
+		this.ativacaoClienteService = ativacaoClienteService;
+	}
 
 	@GetMapping("/hello")
 	@ResponseBody
 	public String hello() {
+		Cliente joao = new Cliente("Joao", "joao@email.com", "92903398383");
+		ativacaoClienteService.ativar(joao);
 		return "Hello!";
 	}
 	
